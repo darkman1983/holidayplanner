a283d6dfbd30d6800a3dc90aaded3b424d818f8b
DarkMan <der_billyboy@hotmail.com>
2016-05-02 22:43:32 +0200

    - Romoved some unused fonts
    * Edited an example file to get in touch with pdf syntax

--- classes/pdf/examples/example_065.php
+++ classes/pdf/examples/example_065.php
@@ -32,24 +32,25 @@ require_once('tcpdf_include.php');
 class MYPDF extends TCPDF {
 
 	//Page header
-	public function Header() {
-		// Logo
-		$image_file = K_PATH_IMAGES.'logo_example.jpg';
-		$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+	public function Header() {	   
+	    // Logo
+		$image_file = K_PATH_IMAGES.'itslogo.jpg';
+		$this->Image($image_file, 25, 10, 40, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 		// Set font
-		$this->SetFont('helvetica', 'B', 20);
+		$this->SetFont('arial', 'B', 20);
 		// Title
-		$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+		$this->Cell(120, 10, 'Urlaubsplaner - Urlaubsantrag '.date("Y", time()), 0, false, 'C', 0, '', 0, false, 'M', 'M');
 	}
 
 	// Page footer
 	public function Footer() {
 		// Position at 15 mm from bottom
 		$this->SetY(-15);
+		
 		// Set font
-		$this->SetFont('helvetica', 'I', 8);
+		$this->SetFont('arial', 'I', 8);
 		// Page number
-		$this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+		$this->Cell(0, 15, 'Version 1.0 - Seite '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
 	}
 }
 
@@ -58,9 +59,9 @@ $pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8',
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 003');
-$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetAuthor('Timo Stepputtis');
+$pdf->SetTitle('IT-Solutions Urlaubsplaner');
+$pdf->SetSubject('Urlaubsantrag '.date("Y", time()));
 $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
 // set default header data
@@ -74,7 +75,7 @@ $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 // set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetMargins(PDF_MARGIN_LEFT+10, PDF_MARGIN_TOP+5, PDF_MARGIN_RIGHT-30);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
@@ -85,28 +86,39 @@ $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 // set some language-dependent strings (optional)
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-	require_once(dirname(__FILE__).'/lang/eng.php');
+if (@file_exists(dirname(__FILE__).'/lang/ger.php')) {
+	require_once(dirname(__FILE__).'/lang/ger.php');
 	$pdf->setLanguageArray($l);
 }
 
 // ---------------------------------------------------------
 
 // set font
-$pdf->SetFont('times', 'BI', 12);
+$pdf->SetFont('arial', 'BI', 12);
 
 // add a page
 $pdf->AddPage();
 
-// set some text to print
-$txt = <<<EOD
-TCPDF Example 003
+// Print a table
 
-Custom page header and footer are defined by extending the TCPDF class and overriding the Header() and Footer() methods.
-EOD;
+$html = '
+<table border="1" cellspacing="0" cellpadding="4" style="width:100%">
+	<tr>
+      <td style="font-weight:bold;width: 8%">Name</td>
+	  <td style="width: 25%">Max Mustermann</td>
+      <td style="font-weight:bold">Personal-Nr.</td>
+	  <td style="width: 25%">005</td>
+	</tr>
+    <tr>
+      <td colspan="4"><table border="1" cellspacing="0" cellpadding="4" style="width:100%"><tr><td style="width: 35%">Resturlaub f&uuml;r 2015</td style="width: 15%"><td></td><td style="width: 8%">5</td><td style="width: 25%">Arbeitstage</td></tr></table></td>
+	</tr>
+</table>';
 
-// print a block of text using Write()
-$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+// output the HTML content
+$pdf->writeHTML($html, true, false, true, false, '');
+
+// reset pointer to the last page
+$pdf->lastPage();
 
 // ---------------------------------------------------------
 
