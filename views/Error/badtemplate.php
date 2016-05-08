2599b1449ad53d4d66c4c4d53ae73c8efdcdbd36
DarkMan <der_billyboy@hotmail.com>
2016-05-08 16:24:30 +0200

    * :-)

--- views/Error/badtemplate.php
+++ views/Error/badtemplate.php
@@ -1,2 +1,15 @@
+<?php 
+if (!$viewModel->get('ajax')) {
+  include 'views/header.php';
+  include 'views/navbar.php';
+}
+?>
+
 <h1>Fatal MVC Error</h1>
 <p>The controller action you requested is configured to access <strong>/views/<?php echo $template; ?>.php</strong> as a view template, but this does not exist. Please make sure this file exists in /views or change the template name argument in your controller action's $this->returnView() method call to an existing template.</p>
+
+<?php 
+if (!$viewModel->get('ajax')) {
+  include 'views/footer.php';
+}
+?>
--- views/Error/badview.php
+++ views/Error/badview.php
@@ -1,2 +1,15 @@
+<?php 
+if (!$viewModel->get('ajax')) {
+  include 'views/header.php';
+  include 'views/navbar.php';
+}
+?>
+
 <h1>Fatal MVC Error</h1>
 <p>The controller action you requested does not have a view. This should exist as <strong><?php echo $this->viewFile; ?></strong>. Please create this file.
+
+<?php 
+if (!$viewModel->get('ajax')) {
+  include 'views/footer.php';
+}
+?>
